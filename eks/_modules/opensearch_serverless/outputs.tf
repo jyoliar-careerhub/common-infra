25259db3e5efca4fb08d0c7197c8814ef2142583
jyo liar <jyo-liar@gmail.com>
2025-04-22 16:52:36 +0900

    opensearch를 serverless로 변경

---
+++ eks/_modules/opensearch_serverless/outputs.tf
@@ -0,0 +1,7 @@
+output "opensearch_endpoint" {
+  value = aws_opensearchserverless_collection.this.collection_endpoint
+}
+
+output "opensearch_dashboard_endpoint" {
+  value = aws_opensearchserverless_collection.this.dashboard_endpoint
+}
---
+++ eks/_modules/opensearch_serverless/variables.tf
@@ -0,0 +1,12 @@
+variable "name" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "subnet_ids" {
+  type = list(string)
+}
+
